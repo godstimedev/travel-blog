52a01b6e257203a58c3bfaf6e8326853927e8a85
Godstime Agholor <agholorgodstime18@gmail.com>
2022-05-27 17:43:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Country from "./Country";
+import data from "./data";
 
-function App() {
+export default function App() {
+  const countries = data.map((item) => {
+    return (
+      <Country
+        img={item.imageUrl}
+        title={item.title}
+        location={item.location}
+        map={item.googleMapsUrl}
+        startDate={item.startDate}
+        endDate={item.endDate}
+        description={item.description}
+      />
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {countries}
+    </>
   );
 }
-
-export default App;
